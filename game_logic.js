bcc41db7c5369cbbc0fd3b56cb5eaae72b1f7499
LiarleyCodie <liarley.personal@gmail.com>
2022-06-07 19:44:59 -0300

    When press Space is compared if Marker Position are in the same position of any card

--- game_logic.js
+++ game_logic.js
@@ -21,32 +21,48 @@ document.addEventListener("keypress", e => {
     if (e.key == "ArrowLeft") {
         if (markerPosition.x > 0) {
             markerPosition.x -= 1
-            reference()
+            cardMarkerReference()
         }
     }
     if (e.key == "ArrowRight") {
-        if (markerPosition.x < cardsList[0].length) {
+        if (markerPosition.x < cardsList[0].length - 1) {
             markerPosition.x += 1
-            reference()
+            cardMarkerReference()
         }
     }
     if (e.key == "ArrowUp") {
         if (markerPosition.y > 0) {
             markerPosition.y -= 1
-            reference()
+            cardMarkerReference()
         }
     }
     if (e.key == "ArrowDown") {
-        if (markerPosition.y < cardsList.length) {
+        if (markerPosition.y < cardsList.length - 1) {
             markerPosition.y += 1
-            reference()
+            cardMarkerReference()
+        }
+    }
+
+    if (e.key == " ") {
+        console.log("Valor")
+        if (markerPosition.x == cardsList[markerPosition.y][markerPosition.x].x) {
+
+            selectedCardReference()
         }
     }
 })
 
-function reference() {
+function cardMarkerReference() {
     console.clear()
-    console.log("Selected Card:", markerPosition)
+    console.log("Selected Card Marker Position:", markerPosition)
 }
 
-reference()
+function selectedCardReference() {
+    console.clear()
+    cardMarkerReference()
+    console.log(`O Cartão selecionado está na:
+    Linha (x): ${cardsList[markerPosition.y][markerPosition.x].y}
+    Coluna (y): ${cardsList[markerPosition.y][markerPosition.x].x}`)
+}
+
+cardMarkerReference()
